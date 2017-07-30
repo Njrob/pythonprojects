b2d6c4c7447913d56503dac29c3dc52e1cf788ed
Njrob <robertsnj91@gmail.com>
2017-07-30 17:01:57 -0400

    Delete myPets1.py

--- myPets1.py
+++
@@ -1,7 +0,0 @@
-myPets = ['Zophie', 'Ralph', 'Richie Aprile']
-print('Enter a pet name: ')
-name = input()
-if name in myPets:
-    print("Your pet's name is on the list.")
-else:
-    print("Your pet's name is not on the list.")
